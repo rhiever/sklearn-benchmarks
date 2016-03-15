293d6429f7410febc704287cda6ff19ae71bd965
Randy Olson <olsonran@msu.edu>
2016-03-15 00:00:55 -0400

    Update SVC.py

--- model_code/SVC.py
+++ model_code/SVC.py
@@ -9,11 +9,16 @@ dataset = sys.argv[1]
 # Read the data set into memory
 input_data = pd.read_csv(dataset, compression='gzip', sep='\t')
 
-for (C, gamma, shrinking, kernel, degree) in itertools.product([0.01, 0.1, 0.5, 1.0, 10.0, 50.0, 100.0],
-                                                               [0.01, 0.1, 0.5, 1.0, 10.0, 50.0, 100.0],
-                                                               [True, False],
+for (C, gamma, kernel, degree) in itertools.product([0.01, 0.1, 0.5, 1.0, 10.0, 50.0, 100.0],
+                                                               [0.01, 0.1, 0.5, 1.0, 10.0, 50.0, 100.0, 'auto'],
                                                                ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'],
                                                                [2, 3, 4]):
+    if kernel != 'poly' and degree > 2:
+        continue
+
+    if kernel not in ['rbf', 'poly', 'sigmoid'] and gamma != 'auto':
+        continue
+
     for dataset_repeat in range(1, 31):
         # Divide the data set into a training and testing sets, each time with a different RNG seed
         training_indices, testing_indices = next(iter(StratifiedShuffleSplit(input_data['class'].values,
@@ -30,7 +35,7 @@ for (C, gamma, shrinking, kernel, degree) in itertools.product([0.01, 0.1, 0.5, 
 
         # Create and fit the model on the training data
         try:
-            clf = SVC(C=C, gamma=gamma, shrinking=shrinking, kernel=kernel, degree=degree)
+            clf = SVC(C=C, gamma=gamma, kernel=kernel, degree=degree)
             clf.fit(training_features, training_classes)
             testing_score = clf.score(testing_features, testing_classes)
         except:
