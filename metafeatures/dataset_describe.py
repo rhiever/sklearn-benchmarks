eaaaedef7e6a86689e20bf1931db591706bb4ec9
Harsh Nisar <nisar.harsh@gmail.com>
2016-03-18 03:43:13 +0530

    Added correlation with target stats, class_probality stat for target and symbol counts for categorical variables. Also lazy loading

--- metafeatures/dataset_describe.py
+++ metafeatures/dataset_describe.py
@@ -26,23 +26,21 @@ class Dataset:
     """
     df = None
     df_encoded = None
-    categorical_col = None
+    categorical_cols = None
     dependent_col = None
     prediction_type = None
     independent_col = None
-    def __init__(self, df, prediction_type = None, dependent_col = None,categorical_col = None):
+    def __init__(self, df, prediction_type = None, dependent_col = None,categorical_cols = None):
         
         self.df = df
         self._set_dependent_col(dependent_col)
-        self._set_categorical_columns(categorical_col)
+        self._set_categorical_columns(categorical_cols)
         self._set_prediction_type(prediction_type)
 
         self.independent_col = list(set(self.df.columns.tolist()) - set(self.dependent_col))
         self._categorical_column_encoder()
 
-        # setups
-        self._setup_correlations()
-
+        
     def _set_dependent_col(self, dependent_col):
         """ if nothing given, set the last column in the frame as
         the dependent column."""
@@ -66,21 +64,23 @@ class Dataset:
         else:
             self.prediction_type = prediction_type
 
-    def _set_categorical_columns(self, categorical_col):
+    def _set_categorical_columns(self, categorical_cols):
         #TODO: Need to test if the columns exist in the df
         #TODO: Add logic in case user doesn't specify the cols
-        if categorical_col == None:
+        if categorical_cols == None:
             num_cols = self.df._get_numeric_data().columns
             cat_cols = list(set(self.df.columns) - set(num_cols) - set([self.dependent_col]))
-            self.categorical_col = cat_cols
+            self.categorical_cols = cat_cols
+            ## empty list in case of no categorical columns.
+
         else:
-            self.categorical_col = categorical_col
+            self.categorical_cols = categorical_cols
 
     
     def _categorical_column_encoder(self):
         """ Assumes all categorical variables are nominal and not
         ordinal """
-        categorical_cols = self.categorical_col
+        categorical_cols = self.categorical_cols
         
         self.df_encoded = self.df.copy()
 
@@ -107,7 +107,7 @@ class Dataset:
     def n_categorical(self):
         """number of categorical variables excluding the dependent."""
         #todo: can be converted to ratio by total number of columns.
-        return len(self.categorical_col)
+        return len(self.categorical_cols)
 
     def n_numerical(self):
         """number of categorical variables excluding the dependent."""
@@ -128,22 +128,25 @@ class Dataset:
     #todo: ratop_rowcol_post_encoding
 
 
-    ## Correlation related measures
+    #----------------------------------------------------------------------
+    # Correlation related
     corr_with_dependent = None
-    abs_corr_with_dependent = None
 
-    def _setup_correlations(self):
+    def _get_corr_with_dependent(self):
         """Called from init. Sets up data for correlation related meta-features.
         #todo: take-call - Should I make different classes/modules for
         different types of meta-features? Eg. Correlation, Entropy"""
         
         #Correlation with dependent variable only make sense for regression problems
         if self.prediction_type == 'regression':
-            self.corr_with_dependent = self.df_encoded.corr()[self.dependent_col]
-            self.corr_with_dependent = self.corr_with_dependent.loc[self.corr_with_dependent.index!=self.dependent_col]
-            self.abs_corr_with_dependent = self.corr_with_dependent.abs()
-    
-    def max_abs_corr_with_dependent(self):
+            if self.corr_with_dependent!=None:
+                return self.corr_with_dependent
+            else:
+                self.corr_with_dependent = self.df_encoded.corr()[self.dependent_col]
+                self.corr_with_dependent = self.corr_with_dependent.loc[self.corr_with_dependent.index!=self.dependent_col]
+                return self.corr_with_dependent
+
+    def corr_with_dependent_abs_max(self):
         """ max absolute pearson correlation with dependent variable
         returns np.nan for classificaiton problems. Uses df_encoded
         ie dataframe with categorical columns encoded automatically.
@@ -151,9 +154,10 @@ class Dataset:
         if self.prediction_type == 'classification':
             return np.nan
         else:
-            return self.abs_corr_with_dependent.max()
+            abs_corr_with_dependent = self._get_corr_with_dependent().abs()
+            return abs_corr_with_dependent.max()
     
-    def min_abs_corr_with_dependent(self):
+    def corr_with_dependent_abs_min(self):
         """ min absolute pearson correlation with dependent variable
         returns np.nan for classificaiton problems. Uses df_encoded
         ie dataframe with categorical columns encoded automatically.
@@ -161,7 +165,191 @@ class Dataset:
         if self.prediction_type == 'classification':
             return np.nan
         else:
-            return self.abs_corr_with_dependent.min()
+            abs_corr_with_dependent = self._get_corr_with_dependent().abs()
+            return abs_corr_with_dependent.min()    
 
-    ## Entropy related measures.
+
+    def corr_with_dependent_abs_mean(self):
+        """ mean absolute pearson correlation with dependent variable
+        returns np.nan for classificaiton problems. Uses df_encoded
+        ie dataframe with categorical columns encoded automatically.
+        """
+        if self.prediction_type == 'classification':
+            return np.nan
+        else:
+            abs_corr_with_dependent = self._get_corr_with_dependent().abs()
+            return abs_corr_with_dependent.mean()    
+
+    def corr_with_dependent_abs_median(self):
+        """ median absolute pearson correlation with dependent variable
+        returns np.nan for classificaiton problems. Uses df_encoded
+        ie dataframe with categorical columns encoded automatically.
+        """
+        if self.prediction_type == 'classification':
+            return np.nan
+        else:
+            abs_corr_with_dependent = self._get_corr_with_dependent().abs()
+            return abs_corr_with_dependent.median()    
+
+
+
+    def corr_with_dependent_abs_std(self):
+        """ std absolute pearson correlation with dependent variable
+        returns np.nan for classificaiton problems. Uses df_encoded
+        ie dataframe with categorical columns encoded automatically.
+        """
+        if self.prediction_type == 'classification':
+            return np.nan
+        else:
+            abs_corr_with_dependent = self._get_corr_with_dependent().abs()
+            return abs_corr_with_dependent.std(ddof = 1)    
+
+
+
+    def corr_with_dependent_abs_25p(self):
+        """ median absolute pearson correlation with dependent variable
+        returns np.nan for classificaiton problems. Uses df_encoded
+        ie dataframe with categorical columns encoded automatically.
+        """
+        if self.prediction_type == 'classification':
+            return np.nan
+        else:
+            abs_corr_with_dependent = self._get_corr_with_dependent().abs()
+            return np.nanpercentile(abs_corr_with_dependent, 25)   
+
+
+
+    def corr_with_dependent_abs_75p(self):
+        """ median absolute pearson correlation with dependent variable
+        returns np.nan for classificaiton problems. Uses df_encoded
+        ie dataframe with categorical columns encoded automatically.
+        """
+        if self.prediction_type == 'classification':
+            return np.nan
+        else:
+            abs_corr_with_dependent = self._get_corr_with_dependent().abs()
+            return np.nanpercentile(abs_corr_with_dependent, 75)
+
+    #----------------------------------------------------------------------
+    # Class probablity related
+    class_probablities = None
+    def _get_class_probablity(self):
+        if self.class_probablities is None:
+            dependent_col = self.df[self.dependent_col]
+            class_counts = dependent_col.value_counts()
+            self.class_probablities = class_counts/self.n_rows()
+            return self.class_probablities
+        else:
+            return self.class_probablities
+
+    def class_prob_min(self):
+        if self.prediction_type=='regression':
+            return np.nan
+        else:
+            class_probablities = self._get_class_probablity()
+            return class_probablities.min()
+    
+    def class_prob_max(self):
+        if self.prediction_type=='regression':
+            return np.nan
+        else:
+            class_probablities = self._get_class_probablity()
+            return class_probablities.max()
+    
+    def class_prob_std(self):
+        if self.prediction_type=='regression':
+            return np.nan
+        else:
+            class_probablities = self._get_class_probablity()
+            return class_probablities.std(ddof = 1)    
+    
+    def class_prob_mean(self):
+        if self.prediction_type=='regression':
+            return np.nan
+        else:
+            class_probablities = self._get_class_probablity()
+            return class_probablities.mean()    
+    
+    def class_prob_median(self):
+        if self.prediction_type=='regression':
+            return np.nan
+        else:
+            class_probablities = self._get_class_probablity()
+            return class_probablities.median()
+
+    #----------------------------------------------------------------------
+    # Symbols related - All the categorical columns
+
+    symbol_counts_dict = {}
+    def _get_symbols_per_category(self):
+        """
+        Sets an dictionary with number of symbols per categorical 
+        column using categorical_cols info.
+        """
+
+
+        if not self.symbol_counts_dict:
+            for column in self.categorical_cols:
+                self.symbol_counts_dict[column] = self.df[column].dropna().unique().shape[0]
+            return self.symbol_counts_dict
+        else:
+            return self.symbol_counts_dict
+
+    def symbols_mean(self):
+        """ Average symbols per columns """
+
+        symbol_counts_dict = self._get_symbols_per_category()
+        ## None is for checking empty, no categorical columns
+        
+        if not symbol_counts_dict:
+            return np.nan
+        symbol_counts = symbol_counts_dict.values()
+
+        return np.nanmean(symbol_counts)
+
+
+    def symbols_std(self):
+        """ std of symbols per columns """
+        symbol_counts_dict = self._get_symbols_per_category()
+        ## None is for checking empty, no categorical columns
+        if not symbol_counts_dict:
+            return np.nan
+        symbol_counts = symbol_counts_dict.values()
+
+        return np.nanstd(symbol_counts, ddof = 1)
+
+    
+    def symbols_min(self):
+        """ Average symbols per columns """
+        symbol_counts_dict = self._get_symbols_per_category()
+        ## None is for checking empty, no categorical columns
+        if not symbol_counts_dict:
+            return np.nan
+        symbol_counts = symbol_counts_dict.values()
+
+        return np.min(symbol_counts)   
+
+    def symbols_max(self):
+        """ Average symbols per columns """
+        symbol_counts_dict = self._get_symbols_per_category()
+        ## None is for checking empty, no categorical columns
+
+        if not symbol_counts_dict:
+            return np.nan
+        symbol_counts = symbol_counts_dict.values()
+
+        return np.max(symbol_counts)
+
+    def symbols_sum(self):
+        """ Sum of symbols per column """
+        symbol_counts_dict = self._get_symbols_per_category()
+        ## None is for checking empty, no categorical columns
+        if not symbol_counts_dict:
+            return np.nan
+
+        symbol_counts = symbol_counts_dict.values()
+
+        return np.sum(symbol_counts)  
+
+    ##todo: Note we can evaluate symbol probabilities too.
 
--- metafeatures/tests_dataset_describe.py
+++ metafeatures/tests_dataset_describe.py
@@ -24,6 +24,10 @@ class Dataset_Describe(unittest.TestCase):
         result = self.iris.n_columns()
         self.assertEqual(5, result)
  
+    def test_if_self_categorical_cols_is_zero_iris(self):
+        result = self.iris.categorical_cols
+        self.assertEqual([], result)
+
     def test_number_of_categorical_vars(self):
         result = self.iris.n_categorical()
         self.assertEqual(0, result)
@@ -50,17 +54,80 @@ class Dataset_Describe(unittest.TestCase):
 
 
     def test_max_corr_with_dependent_classification(self):
-        result = self.iris.max_abs_corr_with_dependent()
+        result = self.iris.corr_with_dependent_abs_max()
         self.assertTrue(math.isnan(result))
 
     def test_max_corr_with_dependent_regression(self):
-        result = self.tips.max_abs_corr_with_dependent()
+        result = self.tips.corr_with_dependent_abs_max()
         self.assertAlmostEqual(0.675,result, places = 2)
 
     def test_min_corr_with_dependent_regression(self):
-        result = self.tips.min_abs_corr_with_dependent()
+        result = self.tips.corr_with_dependent_abs_min()
         self.assertAlmostEqual(0.002, result, places = 2)
 
+
+    def test_25p_corr_with_dependent_regression(self):
+        result = self.tips.corr_with_dependent_abs_25p()
+        self.assertAlmostEqual(0.055, result, places = 2)
+
+
+    def test_mean_corr_with_dependent_regression(self):
+        result = self.tips.corr_with_dependent_abs_mean()
+        self.assertAlmostEqual(0.18, result, places = 2)
+
+
+    def test_median_corr_with_dependent_regression(self):
+        result = self.tips.corr_with_dependent_abs_median()
+        self.assertAlmostEqual(0.095, result, places = 2)
+
+
+    def test_75p_corr_with_dependent_regression(self):
+        result = self.tips.corr_with_dependent_abs_75p()
+        self.assertAlmostEqual(0.125, result, places = 2)
+
+    def test_std_corr_with_dependent_regression(self):
+        result = self.tips.corr_with_dependent_abs_std()
+        self.assertAlmostEqual(0.234, result, places = 2)
+
+
+    def test_class_probablity_total_suite(self):
+        ## evaluating for regression problem.
+        result = self.tips.class_prob_min()
+        self.assertTrue(math.isnan(result))
+        ## evaluating for iris dataset.
+        ## not really a useful test it seems, should get another dataset.
+        ## Also not a unit test.
+
+        self.assertAlmostEqual(0.33, self.iris.class_prob_min(), places = 2)
+        self.assertAlmostEqual(0.33, self.iris.class_prob_max(), places = 2)
+        self.assertAlmostEqual(0, self.iris.class_prob_std(), places = 2)
+        self.assertAlmostEqual(0.33, self.iris.class_prob_mean(), places = 2)
+        self.assertAlmostEqual(0.33, self.iris.class_prob_median(), places = 2)
+
+
+    def test_class_symbols_suite(self):
+        mean = 2.5
+        std = 1
+        min_ = 2
+        max_ = 4
+
+        ## Iris has no nominal variables.
+
+        result = self.iris.symbols_sum()
+        self.assertTrue(math.isnan(result))
+        
+        ## evaluating for tips dataset.
+        ## Also not a unit test.
+        symbol_counts = self.tips._get_symbols_per_category()
+
+        self.assertEqual(symbol_counts['sex'], 2)
+        self.assertEqual(symbol_counts['smoker'], 2)
+
+        self.assertAlmostEqual(max_, self.tips.symbols_max(), places = 2)
+        self.assertAlmostEqual(min_, self.tips.symbols_min(), places = 2)
+        self.assertAlmostEqual(std, self.tips.symbols_std(), places = 2)
+        self.assertAlmostEqual(mean, self.tips.symbols_mean(), places = 2)
+                
 
 
 if __name__ == '__main__':
