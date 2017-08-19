e2bfea44b15e67650027c36ce0b6b83c2d7ba599
Randy Olson <rso@randalolson.com>
2017-08-18 20:36:58 -0400

    Refine SVC parameters
    Don’t need to run SVC with the linear kernel due to LinearSVC.
    precomputed kernel is not useful here.

--- model_code/SVC.py
+++ model_code/SVC.py
@@ -13,7 +13,7 @@ pipeline_parameters = {}
 
 C_values = [0.01, 0.1, 0.5, 1., 10., 50., 100.]
 gamma_values = [0.01, 0.1, 0.5, 1., 10., 50., 100., 'auto']
-kernel_values = ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']
+kernel_values = ['poly', 'rbf', 'sigmoid']
 degree_values = [2, 3]
 coef0_values = [0., 0.1, 0.5, 1., 10., 50., 100.]
 random_state = [324089]
@@ -22,6 +22,6 @@ all_param_combinations = itertools.product(C_values, gamma_values, kernel_values
 pipeline_parameters[SVC] = \
    [{'C': C, 'gamma': gamma, 'kernel': kernel, 'degree': degree, 'coef0': coef0, 'random_state': random_state}
      for (C, gamma, kernel, degree, coef0, random_state) in all_param_combinations
-     if not (kernel != 'poly' and degree > 2) and not (kernel not in ['rbf', 'poly', 'sigmoid'] and gamma != 'auto') and not (kernel not in ['poly', 'sigmoid'] and coef0 != 0.0)]
+     if not (kernel != 'poly' and degree > 2) and not (kernel not in ['poly', 'sigmoid'] and coef0 != 0.0)]
 
 evaluate_model(dataset, pipeline_components, pipeline_parameters)
